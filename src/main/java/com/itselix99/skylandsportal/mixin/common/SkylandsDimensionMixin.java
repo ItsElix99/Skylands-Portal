f006e33c446099a175dc34d98ca50e8a414516dc
Elix <elixgamerbr99@gmail.com>
2026-04-12 21:46:38 -0300

    Add config option to generate Overworld biomes in Skylands

--- src/main/java/com/itselix99/skylandsportal/mixin/common/SkylandsDimensionMixin.java
+++ src/main/java/com/itselix99/skylandsportal/mixin/common/SkylandsDimensionMixin.java
@@ -1,13 +1,18 @@
 package com.itselix99.skylandsportal.mixin.common;
 
+import com.itselix99.skylandsportal.config.SkylandsPortalConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvironmentInterface;
 import net.minecraft.client.resource.language.TranslationStorage;
+import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.SkylandsDimension;
 import net.modificationstation.stationapi.api.client.world.dimension.TravelMessageProvider;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @EnvironmentInterface(value = EnvType.CLIENT, itf = TravelMessageProvider.class)
 @Mixin(SkylandsDimension.class)
@@ -23,5 +28,13 @@ public class SkylandsDimensionMixin extends Dimension implements TravelMessagePr
     @Override
     public String getLeavingTranslationKey() {
         return TranslationStorage.getInstance().get("skylands.leaving");
+    }
+
+
+    @Inject(method = "initBiomeSource", at = @At("TAIL"))
+    private void sp_overworldBiomesInSky(CallbackInfo ci) {
+        if (SkylandsPortalConfig.SPConfig.overworldBiomesInSky) {
+            this.biomeSource = new BiomeSource(this.world);
+        }
     }
 }
