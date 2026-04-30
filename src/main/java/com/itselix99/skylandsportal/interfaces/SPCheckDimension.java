0b78456f2967bdd8853da0f7e90998dad4c4f027
Elix <elixgamerbr99@gmail.com>
2026-04-30 01:14:34 -0300

    Clean branch

--- src/main/java/com/itselix99/skylandsportal/interfaces/SPCheckDimension.java
+++
@@ -1,6 +0,0 @@
-package com.itselix99.skylandsportal.interfaces;
-
-public interface SPCheckDimension {
-    void sp_setInSkylandsPortalBlock(boolean bl);
-    boolean sp_isInSkylandsPortalBlock();
-}
